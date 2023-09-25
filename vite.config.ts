5f791e309685b4f355b0f5e09b4d0524836c7b95
Ruslan Salmanov <flainof@gmail.com>
2023-09-25 22:51:12 +0300

    fixed env

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    define: {
+        BASE_URL: process.env.VITE_BASEURL,
+        SECOND_URL: process.env.VITE_SECONDURL,
+        THIRD_URL: process.env.VITE_THIRDURL,
+    },
+});
